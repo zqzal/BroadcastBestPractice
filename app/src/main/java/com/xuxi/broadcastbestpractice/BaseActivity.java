88d3b6932a67a6076bbe4048e9019126436b85e3
zqzal <936952020@qq.com>
2019-08-02 11:36:47 +0800

    openFileInput方法新增

--- app/src/main/java/com/xuxi/broadcastbestpractice/BaseActivity.java
+++ app/src/main/java/com/xuxi/broadcastbestpractice/BaseActivity.java
@@ -11,6 +11,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+
 public class BaseActivity extends AppCompatActivity {
 
     private ForceOfflineReceiver receiver;
@@ -19,6 +21,11 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityCollector.addActivity(this);
+        try {
+            openFileInput("fefe");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
