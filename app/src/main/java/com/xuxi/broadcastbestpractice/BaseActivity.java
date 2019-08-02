122aa8d535ee683b8203947c54938b8e1f67c199
zqzal <936952020@qq.com>
2019-08-02 09:05:58 +0800

    First command commit.

---
+++ app/src/main/java/com/xuxi/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,72 @@
+package com.xuxi.broadcastbestpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver,intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (receiver != null){
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        ActivityCollector.removeActivity(this);
+
+    }
+
+    class  ForceOfflineReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll(); //销毁所有活动
+                    Intent i = new Intent(context,LoginActivity.class);
+                    context.startActivity(i);//重新启动LoginActivity
+
+                }
+            });
+
+            builder.show();
+        }
+    }
+
+}
