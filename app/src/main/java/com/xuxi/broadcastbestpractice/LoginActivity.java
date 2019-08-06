f7b0272d7d35a79c1925e2595e7e36fff3183db6
zqzal <936952020@qq.com>
2019-08-06 13:46:18 +0800

    获取Share数据

--- app/src/main/java/com/xuxi/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/xuxi/broadcastbestpractice/LoginActivity.java
@@ -3,29 +3,58 @@ package com.xuxi.broadcastbestpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends BaseActivity {
 
+    private static final String TAG = "LoginActivity";
     private EditText accountEdit;
 
     private EditText passwordEdit;
 
     private Button login;
 
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
+
+    private CheckBox rememberPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+
         accountEdit = findViewById(R.id.account);
         passwordEdit = findViewById(R.id.password);
         login = findViewById(R.id.login);
+
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberPass = findViewById(R.id.remember_pass);
+        boolean isRemember = pref.getBoolean("remember_password",false);
+        if (isRemember){
+            //将账号和密码都设置到文本框中
+            String account = pref.getString("account","");
+            String password = pref.getString("password","");
+
+            Log.d(TAG, "onCreate: " + account);
+            Log.d(TAG, "onCreate: " + password);
+
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,6 +62,19 @@ public class LoginActivity extends BaseActivity {
                 String password = passwordEdit.getText().toString();
 
                 if (account.equals("admin") && password.equals("123456")){
+
+
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()){ //检查复选框是否被选中
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+
+                    }else {
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
